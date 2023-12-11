2a0eac14af10fc4d29eae416c58edac4e33c4714
Thales Raymond <thalesfraymond@gmail.com>
2023-12-11 19:07:28 -0300

    choose direction of the the attack during combo

--- Assets/Scripts/PlayerPrimaryAttackState.cs
+++ Assets/Scripts/PlayerPrimaryAttackState.cs
@@ -20,11 +20,19 @@ public class PlayerPrimaryAttackState : PlayerState
         if (comboCounter > 2 || Time.time >= timeSinceLastAttack + comboWindow)
             comboCounter = 0;
 
-        this.Player.SetVelocity(this.Player.AttackMovement[comboCounter] * this.Player.FacingDirection, this.Rb.velocity.y);
+        this.Player.SetVelocity(this.Player.AttackMovement[comboCounter] * this.GetAttackDirection(), this.Rb.velocity.y);
 
         this.Player.Anim.SetInteger("ComboCounter", comboCounter);
 
-        this.StateTimer = .1f;        
+        this.StateTimer = .1f;
+    }
+
+    private float GetAttackDirection()
+    {
+        if (xInput != 0)
+            return xInput;
+
+        return this.Player.FacingDirection;
     }
 
     public override void Exit()
