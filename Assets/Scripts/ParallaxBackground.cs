99a8d8a35f37e18c272614440c4601d5a2234770
Thales Raymond <thalesfraymond@gmail.com>
2023-12-14 18:34:48 -0300

    infinite parallax

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -10,18 +10,30 @@ public class ParallaxBackground : MonoBehaviour
     private float parallaxEffect;
 
     private float xPosition;
+
+    private float length;
     void Start()
     {
         this.mainCamera = GameObject.Find("Main Camera");
 
         this.xPosition = transform.position.x;
+
+        this.length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float distanceMoved = this.mainCamera.transform.position.x * (1 - this.parallaxEffect);
+
         float distanceToMove = this.mainCamera.transform.position.x * this.parallaxEffect;
 
         transform.position = new Vector3(this.xPosition + distanceToMove, transform.position.y, transform.position.z);
+
+        if (distanceMoved > this.xPosition + this.length)
+            xPosition = xPosition + length;
+        else if (distanceMoved < this.xPosition - this.length)
+            xPosition = xPosition - length;
+
     }
 }
