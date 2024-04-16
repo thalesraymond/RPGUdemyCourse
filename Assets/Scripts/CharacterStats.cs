910ba9a7756986e4c8b3cfd9a5fb9020fd72c668
Thales Raymond <thalesfraymond@gmail.com>
2024-04-16 20:48:27 -0300

    Armor and Evasion

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -1,13 +1,22 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
-    public Stat Strength;
-    public Stat Damage;
+    [Header("Major stats")]
+    public Stat Strength; //increases damage
+    public Stat Agility; // increases evasion and crtical hit chance
+    public Stat Intelligence; // increases magic damage
+    public Stat Vitality; // increases health
+
+    [Header("Defensive Stats")]
     public Stat MaxHealthPoints;
+    public Stat Armor;
+    public Stat Evasion;
+
+
+    public Stat Damage;
+
 
     public int CurrentHealthPoints;
 
@@ -19,9 +28,30 @@ public class CharacterStats : MonoBehaviour
 
     public virtual void DoDamage(CharacterStats targetStats)
     {
+        if (this.TargetCanAvoidAttack(targetStats))
+            return;
+
         var totalDamage = Damage.GetValue() + Strength.GetValue();
 
+        totalDamage = this.CheckTargetArmor(targetStats, totalDamage);
+
         targetStats.TakeDamage(totalDamage);
+    }
+
+    private int CheckTargetArmor(CharacterStats targetStats, int totalDamage)
+    {
+        totalDamage -= targetStats.Armor.GetValue();
+
+        totalDamage = Mathf.Clamp(0, totalDamage, int.MaxValue);
+
+        return totalDamage;
+    }
+
+    private bool TargetCanAvoidAttack(CharacterStats targetStats)
+    {
+        var totalEvasion = targetStats.Evasion.GetValue();
+
+        return Random.Range(0, 100) < totalEvasion;
     }
 
     public virtual void TakeDamage(int damage)
