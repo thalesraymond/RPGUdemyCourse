96039bc5790a84e5cccbf9e84749124ad1dc9d3d
Thales Raymond <thalesfraymond@gmail.com>
2024-05-15 18:14:10 -0300

    Item ScriptableObject

---
+++ Assets/Scripts/ItemObject.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour
+{
+    private SpriteRenderer _spriteRenderer;
+    [SerializeField] private ItemData _itemData;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        _spriteRenderer.sprite = _itemData.ItemIcon;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Player>() != null)
+        {
+            Debug.Log("Picked up item" + _itemData.ItemName);
+
+            Destroy(gameObject);
+        }
+
+    }
+}
