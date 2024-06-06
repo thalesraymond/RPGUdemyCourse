edbf42851281cf70f3e5097697f4da11f18b0d09
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2024-06-06 18:04:04 -0300

    enemy stats

--- Assets/Scripts/Stats/EnemyStats.cs
+++ Assets/Scripts/Stats/EnemyStats.cs
@@ -5,12 +5,51 @@ using UnityEngine;
 public class EnemyStats : CharacterStats
 {
     private Enemy _enemy;
-    // Start is called before the first frame update
+    
+    [Header("Level details")]
+    [SerializeField] private int _level = 1;
+    [Range(0f,1f)] [SerializeField] private float _percentageModifier =  .4f;
     protected override void Start()
     {
         base.Start();
 
         _enemy = GetComponent<Enemy>();
+
+        ApplyLevelModifier();
+    }
+
+    private void ApplyLevelModifier()
+    {
+        Modify(this.MaxHealthPoints);        
+        this.CurrentHealthPoints = this.MaxHealthPoints.GetValue();
+        
+        // Do not modify strength, agility, intelligence, vitality for enemies
+        // Modify(this.Strength);
+        // Modify(this.Agility);
+        // Modify(this.Intelligence);
+        // Modify(this.Vitality);
+
+        Modify(this.Damage);
+        Modify(this.FireDamage);
+        Modify(this.IceDamage);
+        Modify(this.LightningDamage);
+
+        Modify(this.Armor);
+        Modify(this.Evasion);
+        Modify(this.MagicResistance); 
+
+        Modify(this.CriticalHitChance);
+        Modify(this.CriticalHitPower);
+    }
+
+    private void Modify(Stat stat)
+    {
+        for(var i = 1; i < _level; i++)
+        {
+            var modifier = stat.GetValue() * _percentageModifier;
+
+            stat.AddModifier(Mathf.RoundToInt(modifier));
+        }
     }
 
     public override void TakeDamage(int damage)
