17e6900c3e8fff354580b6c0ca9b1de83d60ca05
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2024-06-06 19:36:21 -0300

    crafting system basics

--- Assets/Scripts/UI/CraftSlotUI.cs
+++ Assets/Scripts/UI/CraftSlotUI.cs
@@ -11,8 +11,19 @@ public class CraftSlotUI : ItemSlotUI
     }
     public override void OnPointerDown(PointerEventData eventData)
     {
-        base.OnPointerDown(eventData);
+        var equipmentToCraft = this.Item?.ItemData as EquipmentItemData;
 
-        // inventory craft item  data
+        if (equipmentToCraft == null)
+        {
+            Debug.LogWarning("Item is not an equipment item");
+            return;
+        }
+
+        var canCraft = Inventory.Instance.CanCraftItem(equipmentToCraft, equipmentToCraft.CraftingMaterials);
+
+        if(canCraft)
+        {
+            Debug.Log("Here is your item: " + equipmentToCraft.ItemName);
+        }
     }
 }
