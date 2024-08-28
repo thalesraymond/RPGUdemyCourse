fa7bb802c13279318a63b31a66c1151566dc260c
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2024-08-28 18:10:29 -0300

    menu hotkeys

--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -5,19 +5,47 @@ using UnityEngine;
 public class UI : MonoBehaviour
 {
     public ItemToolTipUI ItemToolTipUI;
-
     public StatToolTipUI StatToolTipUI;
+
+    [SerializeField] private GameObject _characterUI;
+    [SerializeField] private GameObject _skillTreeUI;
+    [SerializeField] private GameObject _craftUI;
+    [SerializeField] private GameObject _optionsUI;
 
     // Start is called before the first frame update
     void Start()
     {
         this.ItemToolTipUI = GetComponentInChildren<ItemToolTipUI>(true);
+
+        SwitchTo(null);
+
+        this.ItemToolTipUI.HideTooltip();
+
+        this.StatToolTipUI.HideTooltip();
     }
      
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            SwitchWithKeyTo(this._characterUI);
+        }
+
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            SwitchWithKeyTo(this._craftUI);
+        }
+
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            SwitchWithKeyTo(this._skillTreeUI);
+        }
+
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            SwitchWithKeyTo(this._optionsUI);
+        }
     }
 
     public void SwitchTo(GameObject menu)
@@ -31,5 +59,16 @@ public class UI : MonoBehaviour
             return;
             
         menu.SetActive(true);
+    }
+
+    public void SwitchWithKeyTo(GameObject menu)
+    {
+        if(menu != null && menu.activeSelf)
+        {
+            menu.SetActive(false);
+            return;
+        }
+
+        SwitchTo(menu);
     }
 }
